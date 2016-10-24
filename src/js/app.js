0eb037284fab4adf00e7430f06b572f5625bc11d
Lindsey Adams <linzma87@gmail.com>
2016-10-24 13:23:17 -0400

    HTML beginning structure

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,17 @@
 (function($) {
   "use strict";
 
+  angular
+    .module('inventory', [])
+    .controller('InventoryController', function() {
+
+      this.tax = '5.75%';
+      // this.tax = 0.0575;
+
+      this.allItems = {
+
+      };
+
+    });
+
 })(jQuery);
